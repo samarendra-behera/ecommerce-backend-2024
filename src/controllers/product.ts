befccb406b0e365242a7056e9ca867d18f0e7ca6
@samarendrabehera.papu <samarendrabehera.papu@gmail.com>
2024-02-23 10:38:47 +0530

    completed product create

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -3,18 +3,25 @@ import { TryCatch } from "../middlewares/error.js";
 import { newProductReqBody } from "../types/types.js";
 import { Product } from '../models/product.js';
 import ErrorHandler from '../utils/utility-class.js';
+import { rm } from 'fs';
 
 
 export const newProduct = TryCatch(async(req:Request<{},{},newProductReqBody>, res, next)=>{
     const {name, price, category, stock } = req.body;
-    if (!name || !price || !category || !stock) return next(new ErrorHandler("Please add all fields", 400))
     const photo = req.file;
+    if (!photo) return next(new ErrorHandler("Please Add Photo", 400))
+    if (!name || !price || !category || !stock) {
+        rm(photo.path, ()=>{
+            console.log("Photo Deleted")
+        })
+        return next(new ErrorHandler("Please add all fields", 400))
+    }
     const product = await Product.create({
         name, 
         price, 
         category:category.toLowerCase(), 
         stock, 
-        photo: photo?.path})
+        photo: photo.path})
     return res.status(201).json({
         success: true,
         message: "Product Added Successfully",
--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,11 +1,14 @@
 import multer from "multer";
+import {v4 as uuid} from 'uuid'
 
 const stroage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "uploads");
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname);
+        const id = uuid();
+        const extName = file.originalname.split(".")[1];
+        cb(null, `${id}.${extName}`);
     },
 });
 
