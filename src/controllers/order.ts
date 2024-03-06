6b59fbb2f351529638358daf1e48475cce7882ee
samarendrabehera.papu@gmail.com <samarendrabehera.papu@gmail.com>
2024-03-06 08:36:13 +0530

    order apis create

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,11 +1,13 @@
-import {Request} from "express"
+import { Request } from "express"
 import { TryCatch } from "../middlewares/error.js";
 import { newOrderReqBody } from "../types/types.js";
 import { Order } from "../models/order.js";
 import { invalidateCache, reduceProductStock } from "../utils/featuers.js";
 import ErrorHandler from "../utils/utility-class.js";
+import { myCache } from "../app.js";
+import { json } from "stream/consumers";
 
-export const newOrder = TryCatch(async(req:Request<{}, {}, newOrderReqBody>, res, next)=>{
+export const newOrder = TryCatch(async (req: Request<{}, {}, newOrderReqBody>, res, next) => {
     const {
         shippingInfo,
         user,
@@ -17,16 +19,14 @@ export const newOrder = TryCatch(async(req:Request<{}, {}, newOrderReqBody>, res
         status,
         orderItems
     } = req.body
-    if(!shippingInfo ||
+    if (!shippingInfo ||
         !user ||
         !subTotal ||
         !tax ||
-        !shippingCharges ||
-        !discount ||
         !total ||
         !status ||
         !orderItems
-        ) return next(new ErrorHandler("Please Enter All Fields", 400))
+    ) return next(new ErrorHandler("Please Enter All Fields", 400))
     await Order.create({
         shippingInfo,
         user,
@@ -40,9 +40,62 @@ export const newOrder = TryCatch(async(req:Request<{}, {}, newOrderReqBody>, res
     })
     await reduceProductStock(orderItems);
 
-    await invalidateCache({product:true, admin:true});
+    await invalidateCache({ product: true, admin: true });
     res.status(200).json({
         success: true,
         message: "Order Placed Successfully"
+    })
+})
+
+export const myOrders = TryCatch(async (req, res, next) => {
+    const { id: user } = req.query
+    let orders = [];
+    const key = `my-orders-${user}`;
+
+    if (myCache.has(key)) orders = JSON.parse(myCache.get(key) as string)
+    else {
+        orders = await Order.find({user}).populate("user", "name")
+        myCache.set(key, JSON.stringify(orders))
+    }
+
+    return res.status(200).json({
+        success: true,
+        orders
+    })
+})
+
+export const allOrders = TryCatch(async(req, res, next)=>{
+    const key = "all-orders"
+    
+    let orders = []
+    if(myCache.has(key)) orders = JSON.parse(myCache.get(key) as string)
+    else {
+        orders = await Order.find({}).populate("user", "name")
+        myCache.set(key, JSON.stringify(orders))
+    }
+
+    res.status(200).json({
+        success: true,
+        orders
+    })
+
+})
+
+export const getOrderDetails = TryCatch(async (req, res, next) =>{
+    const {id} = req.params
+    const key = `order-${id}`
+
+    let order;
+
+    if(myCache.has(key)) order = JSON.parse(myCache.get(key) as string)
+    else {
+        order = await Order.findById(id).populate("user", "name")
+        if (!order) return next(new ErrorHandler("Order not Found", 404))
+        myCache.set(key, JSON.stringify(order))
+    }
+
+    res.status(200).json({
+        success: true,
+        order
     })
 })
--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,13 +1,25 @@
 import express from "express"
 
 import {
+    allOrders,
+    getOrderDetails,
+    myOrders,
     newOrder 
 } from '../controllers/order.js'
 import { adminOnly } from "../middlewares/auth.js"
 
 const app = express.Router()
 
-// Routes - /api/v1/order/new
+// Routes - /api/v1/order/new - To Create new Order
 app.post('/new', newOrder)
+
+// Routes - /api/v1/order/my - To Get My Orders
+app.get('/my', myOrders)
+
+// Routes - /api/v1/order/all - To Admin See All Orders are placed 
+app.get('/all', adminOnly , allOrders)
+
+// Routes - /api/v1/order/:id - To Get Single Order Details
+app.route("/:id").get(getOrderDetails)
 
 export default app
