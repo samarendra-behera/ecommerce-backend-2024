70b602379a8685637c5d1a2be4e1873664079533
samarendrabehera.papu@gmail.com <samarendrabehera.papu@gmail.com>
2024-03-08 13:15:22 +0530

    update

--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -4,6 +4,19 @@ import { newCouponReqBody } from '../types/types.js';
 import { Coupon } from '../models/coupon.js';
 import ErrorHandler from '../utils/utility-class.js';
 
+export const applyDiscount = TryCatch(async (req, res, next)=> {
+    const {coupon} = req.query;
+
+    const discount = await Coupon.findOne({code: coupon})
+    
+    if(!discount) return next(new ErrorHandler("Invalid Coupon Code", 400))
+
+    res.status(200).json({
+        success: true,
+        discount: discount.amount
+    })
+})
+
 export const newCoupon = TryCatch(async(req:Request<{}, {}, newCouponReqBody>, res, next)=>{
     const {code, amount} = req.body
 
@@ -14,5 +27,27 @@ export const newCoupon = TryCatch(async(req:Request<{}, {}, newCouponReqBody>, r
     res.status(201).json({
         success: true,
         message: `Coupon ${code} created Successfully`
+    })
+})
+
+export const allCoupons = TryCatch(async (req, res, next)=>{
+    const coupons = await Coupon.find({});
+
+    res.status(200).json({
+        success: true,
+        coupons
+    })
+})
+
+export const deleteCoupon = TryCatch(async (req, res, next)=>{
+    const {id} = req.params;
+
+    const coupon = await Coupon.findByIdAndDelete(id);
+
+    if(!coupon) return next(new ErrorHandler("Invalid Coupon Id", 400))
+
+    res.status(200).json({
+        success: true,
+        message: `Coupon ${coupon?.code} Deleted Successfully`
     })
 })
--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -1,11 +1,26 @@
 import express from 'express'
 import { 
+    allCoupons,
+    applyDiscount,
+    deleteCoupon,
     newCoupon,
 } from '../controllers/payment.js'
+import { adminOnly } from '../middlewares/auth.js'
 
 const app = express.Router()
 
-// route - /api/v1/payment/coupon/new  -> Create New Coupon
-app.post('/coupon/new', newCoupon)
+// route - /api/v1/payment/discount  -> Apply coupon Code
+app.get('/discount', applyDiscount)
+
+// route - /api/v1/payment/coupon/new  -> Create New Coupon (Admin)
+app.post('/coupon/new',adminOnly, newCoupon)
+
+// route - /api/v1/payment/coupon/all  -> Get All Coupons (Admin)
+app.get('/coupon/all',adminOnly, allCoupons)
+
+// route - /api/v1/payment/coupon/:id  -> Get All Coupons (Admin)
+app.delete('/coupon/:id',adminOnly, deleteCoupon)
+
+
 
 export default app;
