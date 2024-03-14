6360d093ac574cf32308e7f002018760a38c544d
@samarendrabehera.papu <samarendrabehera.papu@gmail.com>
2024-03-14 18:15:06 +0530

    update

---
+++ dist/controllers/order.js
@@ -0,0 +1,113 @@
+import { TryCatch } from "../middlewares/error.js";
+import { Order } from "../models/order.js";
+import { invalidateCache, reduceProductStock } from "../utils/featuers.js";
+import ErrorHandler from "../utils/utility-class.js";
+import { myCache } from "../app.js";
+export const myOrders = TryCatch(async (req, res, next) => {
+    const { id: user } = req.query;
+    let orders = [];
+    const key = `my-orders-${user}`;
+    if (myCache.has(key))
+        orders = JSON.parse(myCache.get(key));
+    else {
+        orders = await Order.find({ user }).populate("user", "name");
+        myCache.set(key, JSON.stringify(orders));
+    }
+    return res.status(200).json({
+        success: true,
+        orders
+    });
+});
+export const allOrders = TryCatch(async (req, res, next) => {
+    const key = "all-orders";
+    let orders = [];
+    if (myCache.has(key))
+        orders = JSON.parse(myCache.get(key));
+    else {
+        orders = await Order.find({}).populate("user", "name");
+        myCache.set(key, JSON.stringify(orders));
+    }
+    res.status(200).json({
+        success: true,
+        orders
+    });
+});
+export const getOrderDetails = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const key = `order-${id}`;
+    let order;
+    if (myCache.has(key))
+        order = JSON.parse(myCache.get(key));
+    else {
+        order = await Order.findById(id).populate("user", "name");
+        if (!order)
+            return next(new ErrorHandler("Order not Found", 404));
+        myCache.set(key, JSON.stringify(order));
+    }
+    res.status(200).json({
+        success: true,
+        order
+    });
+});
+export const newOrder = TryCatch(async (req, res, next) => {
+    const { shippingInfo, user, subTotal, tax, shippingCharges, discount, total, status, orderItems } = req.body;
+    if (!shippingInfo ||
+        !user ||
+        !subTotal ||
+        !tax ||
+        !total ||
+        !status ||
+        !orderItems)
+        return next(new ErrorHandler("Please Enter All Fields", 400));
+    await Order.create({
+        shippingInfo,
+        user,
+        subTotal,
+        tax,
+        shippingCharges,
+        discount,
+        total,
+        status,
+        orderItems
+    });
+    await reduceProductStock(orderItems);
+    invalidateCache({ product: true, order: true, admin: true, userId: user, productId: orderItems.map(i => i.productId) });
+    res.status(200).json({
+        success: true,
+        message: "Order Placed Successfully"
+    });
+});
+export const updateOrderStatus = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const order = await Order.findById(id);
+    if (!order)
+        return next(new ErrorHandler("Order not Found", 404));
+    switch (order.status) {
+        case "Processing":
+            order.status = "Shipped";
+            break;
+        case "Shipped":
+            order.status = "Delivered";
+        default:
+            order.status = "Delivered";
+            break;
+    }
+    order.save();
+    invalidateCache({ product: false, order: true, admin: true, userId: order.user, orderId: String(order._id) });
+    res.status(200).json({
+        success: true,
+        message: "Order Processed Successfully"
+    });
+});
+export const cancelOrder = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const order = await Order.findById(id);
+    if (!order)
+        return next(new ErrorHandler("Order not Found", 404));
+    await order.deleteOne();
+    invalidateCache({ product: false, order: true, admin: true, userId: order.user, orderId: String(order._id) });
+    res.status(200).json({
+        success: true,
+        message: "Order Deleted Successfully"
+    });
+});
---
+++ dist/middlewares/error.js
@@ -0,0 +1,19 @@
+export const errorMiddleware = (err, req, res, next) => {
+    err.message || (err.message = "Internal Server Error");
+    err.statusCode || (err.statusCode = 500);
+    if (err.name == "CastError") {
+        err.message = "Invalid ID";
+        err.statusCode = 400;
+    }
+    if (err.name == "ValidationError") {
+        err.message = "Invalid Data Type";
+        err.statusCode = 400;
+    }
+    return res.status(err.statusCode).json({
+        success: false,
+        message: err.message
+    });
+};
+export const TryCatch = (func) => (req, res, next) => {
+    return Promise.resolve(func(req, res, next)).catch(next);
+};
