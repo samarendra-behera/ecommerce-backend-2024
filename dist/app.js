5b940744effb2014fd442e956346fcddff0caa91
@samarendrabehera.papu <samarendrabehera.papu@gmail.com>
2024-03-15 12:27:19 +0530

    update2

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from 'url';
 import NodeCache from "node-cache";
 import { config } from "dotenv";
 import morgan from "morgan";
@@ -15,6 +16,8 @@ import Stripe from "stripe";
 config({
     path: "./.env",
 });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const port = process.env.PORT || 45000;
 const host = process.env.HOST || '127.0.0.1';
 const mongoUrl = process.env.MONGO_URL || "";
