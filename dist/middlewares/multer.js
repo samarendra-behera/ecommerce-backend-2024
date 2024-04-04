ce1d98df7eabd67c085d115a27cad5557db01229
@samarendrabehera.papu <samarendrabehera.papu@gmail.com>
2024-04-04 14:38:29 +0530

    product update

--- dist/middlewares/multer.js
+++ dist/middlewares/multer.js
@@ -1,9 +1,8 @@
 import multer from "multer";
 import { v4 as uuid } from 'uuid';
-import { upload_path } from '../app.js';
 const stroage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, upload_path);
+        cb(null, "uploads");
     },
     filename: function (req, file, cb) {
         const id = uuid();
